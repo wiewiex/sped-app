e3239781a293f3382f4a9233df89ab6078e3f2f3
Jacek Pietrzak <wiewiex@gmail.com>
2023-09-03 18:46:56 +0200

    fake adresses

--- src/components/controllers/Home/Addresses/Addresses.tsx
+++ src/components/controllers/Home/Addresses/Addresses.tsx
@@ -1,5 +1,4 @@
 import { Container } from '@/components/views/styledContainers';
-import { H2, Text } from '@/components/views/styledTexts';
 import { useAppTheme } from '@/utils/theme';
 import Tile from '../Tile';
 
@@ -24,3 +23,46 @@ export default function Addresses() {
     </Tile>
   );
 }
+
+const addresses = [
+  {
+    name: 'Castorama',
+    address: 'Żwirki wigury 4',
+    postCode: '39-890',
+    city: 'Warszawa',
+    country: 'Poland',
+    gps: [37.7749, -122.4194],
+  },
+  {
+    name: 'Przykładowa Firma',
+    address: 'Aleje Jerozolimskie 123',
+    postCode: '00-001',
+    city: 'Warszawa',
+    country: 'Poland',
+    gps: [52.2297, 21.0122],
+  },
+  {
+    name: 'Sklep AGD',
+    address: 'ul. Piotrkowska 56',
+    postCode: '90-001',
+    city: 'Łódź',
+    country: 'Poland',
+    gps: [51.7704, 19.4562],
+  },
+  {
+    name: 'Meble Domowe',
+    address: 'ul. Krakowska 78',
+    postCode: '30-001',
+    city: 'Kraków',
+    country: 'Poland',
+    gps: [50.0647, 19.945],
+  },
+  {
+    name: 'Sklep Elektroniczny',
+    address: 'ul. Nowy Świat 10',
+    postCode: '00-001',
+    city: 'Warszawa',
+    country: 'Poland',
+    gps: [52.2321, 21.016],
+  },
+];

2087f3d3f718bd09558a2e8ca110967fba74758f
Jacek Pietrzak <wiewiex@gmail.com>
2023-09-14 14:02:12 +0200

    rwd

--- src/components/controllers/Layout/Layout.tsx
+++ src/components/controllers/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { Container } from '@/components/views/styledContainers';
-import Navbar from './Navbar';
+import Header from './Header';
 import useWindowSize from '@/hooks/useWindowSize';
 import Sidebar from './Sidebar';
 import Main from './Main';
@@ -9,14 +9,16 @@ export default function Layout() {
 
   return (
     <Container relative id="app-container" mobileWidth="100%">
-      <Navbar />
+      <Header />
       <Container
         mobileWidth="100%"
         flexDirection="row"
         justifyContent="flex-start"
         alignItems="stretch"
       >
-        <Container width="15%">{!isMobile && <Sidebar />}</Container>
+        <Container width="15%" minWidth="12rem">
+          {!isMobile && <Sidebar />}
+        </Container>
         <Container width="85%" mobileWidth="100%">
           <Main />
         </Container>
